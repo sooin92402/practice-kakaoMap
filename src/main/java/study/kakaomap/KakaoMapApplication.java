cddc8378fa45f7b3f5a501d0697f1e77e2b3c55c
sion <sooin92402@gmail.com>
2021-12-13 12:38:32 +0900

    first commit

---
+++ src/main/java/study/kakaomap/KakaoMapApplication.java
@@ -0,0 +1,13 @@
+package study.kakaomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMapApplication.class, args);
+	}
+
+}
